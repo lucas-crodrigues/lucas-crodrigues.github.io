875d5a24ac72b3fbf04e5189031d4082cc374267
lucascr <lcrodrigues1995@gmail.com>
2022-04-04 19:53:55 -0300

    Fix ESLint errors

--- scripts.js
+++ scripts.js
@@ -3,16 +3,14 @@ const dropdown = document.querySelector('.dropdown-menu');
 const close = document.querySelector('.close');
 const closeOption = document.querySelector('.mobile-menu-options');
 
+hamburger.addEventListener('click', () => {
+  dropdown.classList.remove('hide');
+});
 
-hamburger.addEventListener('click',()=>{
-    dropdown.classList.remove('hide');
-})
-
-
-close.addEventListener('click',()=>{
-    dropdown.classList.add('hide');
-})
-
-closeOption.addEventListener('click',()=>{
+close.addEventListener('click', () => {
   dropdown.classList.add('hide');
-})
+});
+
+closeOption.addEventListener('click', () => {
+  dropdown.classList.add('hide');
+});
