b6a3083da0daa274e4cf25caa0d374f84dc10fe0
lucascr <lcrodrigues1995@gmail.com>
2022-04-05 23:41:26 -0300

    Add projects values

--- scripts.js
+++ scripts.js
@@ -1,3 +1,5 @@
+const { url } = require("inspector");
+
 const hamburger = document.querySelector('.menuicon');
 const dropdown = document.querySelector('.dropdown-menu');
 const close = document.querySelector('.close');
@@ -14,3 +16,74 @@ close.addEventListener('click', () => {
 closeOption.addEventListener('click', () => {
   dropdown.classList.add('hide');
 });
+
+const projects = [{
+  id: 1, 
+  projectTitle: 'Multi Post Stories', 
+  description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. " +
+  "Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, "+
+  "when an unknown printer took a galley of type and scrambled it to make a type specimen book. "+
+  "It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent",
+  image: 'Art/popup-image.png',
+  linktolive: ['See Live', 'Art/Icon-Live.svg'],
+  linktosource: ['See Source', 'Art/Icon-Source.svg']
+},
+{
+  id: 2, 
+  projectTitle: 'Multi Post Stories', 
+  description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. " +
+  "Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, "+
+  "when an unknown printer took a galley of type and scrambled it to make a type specimen book. "+
+  "It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent",
+  image: 'Art/popup-image.png',
+  linktolive: ['See Live', 'Art/Icon-Live.svg'],
+  linktosource: ['See Source', 'Art/Icon-Source.svg']
+},
+{
+  id: 3, 
+  projectTitle: 'Multi Post Stories', 
+  description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. " +
+  "Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, "+
+  "when an unknown printer took a galley of type and scrambled it to make a type specimen book. "+
+  "It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent",
+  image: 'Art/popup-image.png',
+  linktolive: ['See Live', 'Art/Icon-Live.svg'],
+  linktosource: ['See Source', 'Art/Icon-Source.svg']
+},
+{
+  id: 4, 
+  projectTitle: 'Multi Post Stories', 
+  description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. " +
+  "Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, "+
+  "when an unknown printer took a galley of type and scrambled it to make a type specimen book. "+
+  "It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent",
+  image: 'Art/popup-image.png',
+  linktolive: ['See Live', 'Art/Icon-Live.svg'],
+  linktosource: ['See Source', 'Art/Icon-Source.svg']
+},
+{
+  id: 5, 
+  projectTitle: 'Multi Post Stories', 
+  description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. " +
+  "Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, "+
+  "when an unknown printer took a galley of type and scrambled it to make a type specimen book. "+
+  "It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent",
+  image: 'Art/popup-image.png',
+  linktolive: ['See Live', 'Art/Icon-Live.svg'],
+  linktosource: ['See Source', 'Art/Icon-Source.svg']
+},
+{
+  id: 6, 
+  projectTitle: 'Multi Post Stories', 
+  description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. " +
+  "Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, "+
+  "when an unknown printer took a galley of type and scrambled it to make a type specimen book. "+
+  "It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent",
+  image: 'Art/popup-image.png',
+  linktolive: ['See Live', 'Art/Icon-Live.svg'],
+  linktosource: ['See Source', 'Art/Icon-Source.svg']
+}
+];
+
+let html = document.querySelector("popup-top").innerHTML;
+document.querySelector("popup-top").innerHTML = `<h4>${projects.id[1].projectTitle}</h4>`;
